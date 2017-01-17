1eb6535f2f4868a7418d4b88adc6638cf9c0ce5a
Unknown <opti.jawsome@gmail.com>
2017-01-17 07:58:24 -0600

    added drive motors to RobotMap

--- src/org/usfirst/frc/team5829/robot/RobotMap.java
+++ src/org/usfirst/frc/team5829/robot/RobotMap.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team5829.robot;
 
+import edu.wpi.first.wpilibj.TalonSRX;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -7,13 +9,8 @@ package org.usfirst.frc.team5829.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
-}
+	TalonSRX frontLeftMotor = new TalonSRX(1);
+	TalonSRX frontRightMotor = new TalonSRX(2);
+	TalonSRX backRightMotor = new TalonSRX(3);
+	TalonSRX backLeftMotor = new TalonSRX(4);
+	}
